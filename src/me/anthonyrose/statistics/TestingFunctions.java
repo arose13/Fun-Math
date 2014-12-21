26e38916adc0fb4eb5ede5ce62a76db3f4155a35
arose13 <anthony_rose12@hotmail.com>
2014-12-21 17:23:41 -0500

    Zscore calculator

--- src/me/anthonyrose/statistics/TestingFunctions.java
+++ src/me/anthonyrose/statistics/TestingFunctions.java
@@ -11,26 +11,32 @@ public class TestingFunctions {
 		// TODO test functions below
 		BasicStatistics mStatistics = new BasicStatistics();
 		ArrayList<Double> pop = new ArrayList<Double>();
-		pop.add((double) 22); //1
-		pop.add((double) 23); //2
-		pop.add((double) 22); //3
-		pop.add((double) 20); //3
-		pop.add((double) 22); //3
-		pop.add((double) 26); //3
-		pop.add((double) 21); //3
-		pop.add((double) 24); //3
-		pop.add((double) 23); //3
-		pop.add((double) 19); //3
+		pop.add((double) 74);
+		pop.add((double) 112);
+		pop.add((double) 124);
+		pop.add((double) 88);
+		pop.add((double) 96);
+		pop.add((double) 96);
+		pop.add((double) 119);
+		pop.add((double) 100);
+		pop.add((double) 103);
+		pop.add((double) 86);
+		pop.add((double) 69);
+		pop.add((double) 92);
+		pop.add((double) 87);
 		
 		double stdev = mStatistics.stdev(pop);
 		double mean = mStatistics.mean(pop);
 		
-		NormalDistn mDistn = new NormalDistn();
+		ZScore mZScore = new ZScore();
+		
+		double z = mZScore.getZfromX(144, 100, 15);
 		
 		System.out.println("time pre  : " + System.currentTimeMillis());
 		System.out.println(mean);
 		System.out.println(stdev);
-		System.out.println(mDistn.normalDistnPDF(23, mean, stdev));
+		System.out.println(z);
+		System.out.println((mZScore.getProbability(z) * 100) + "%");
 		System.out.println("time post : " + System.currentTimeMillis()); 
 	}
 
